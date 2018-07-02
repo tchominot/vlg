ed76c4a1fe2aee3894edee3e7c2fcd57903b60e5
Thibaud Chominot <thibaud.chominot@epita.fr>
2018-07-02 17:23:10 +0200

    [MISC] Fix warning unused size var

--- src/vecutils.c
+++ src/vecutils.c
@@ -6,7 +6,7 @@
 igraph_integer_t vecutils_pick_random(const igraph_vector_t *v) {
   long int size = igraph_vector_size(v);
   assert(size > 0);
-  long int idx = rand() % igraph_vector_size(v);
+  long int idx = rand() % size;
   return igraph_vector_e(v, idx);
 }
 void vecutils_remove_all_occurences(igraph_vector_t *v, igraph_integer_t val) {
