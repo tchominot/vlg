f45650a94cf2a37c2ba33878d40e54f96791103d
Thibaud Chominot <thibaud.chominot@epita.fr>
2018-07-02 16:26:54 +0200

    [SWEEP] Fill in the comment in sweep-core.h

--- src/sweep-core.h
+++ src/sweep-core.h
@@ -5,7 +5,18 @@
 # include "graph-data.h"
 
 /*
-** TODO: details
+** Core underlying function to perform a single sweep.
+** It performs a bfs (igraph_bfs) and updates the data in gd accordingly.
+** All the fields of graph_data are updated to improve the approximation:
+**  - the maximum eccentricity is updated if it is found that start's eccentricity is higher
+**  - the minimum eccentricity is updated if it is found that start's eccentricity is lower
+**  - the lists of vertices (max_ecc_vids, diametral_candidates and central_vertices) are kept up-to-date as well
+**
+** out_end : if not NULL, the last vertex traversed by the bfs is stored here
+** ecc_vids: if not NULL, the vertices in the last level of the bfs are stored in it
+**           The function clears it before adding any vertex to it.
+** parent  : if not NULL, fill it such that parent[i] contains the vid of the parent of i
+** Returns the eccentricity of the start vertex.
 */
 igraph_integer_t sweep(const igraph_t *g, struct graph_data *gd,
                        igraph_integer_t start, igraph_integer_t *out_end,
